a26a2e56f5323be7cfc80eed43c8eb24390e0808
Ne-GH <printfne@gmail.com>
2022-03-04 14:55:27 +0800

    ConfigArch.sh

--- ConfigArch.sh
+++ ConfigArch.sh
@@ -1,33 +1,35 @@
+#!/usr/bin/bash
+
+
+yes | sudo pacman -Syu
 # setfont
 # use command setfont to set font
 # My arch default fonts in /usr/share/kbd/consolefonts
-setfont font_name
-
-
-# install pkgfile command
-pacman -S pkgfile
-
-# install firefox 
-pacman -S firefox
-# install firefox Chinese package
-pacman -S firefox-i18n-zh-cn
+# setfont font_name
 
 # install Graphics driver
 # we can use commat inquire graphics information
-lspci -k | grep -A 2 -E "(VGA|3D)"
+# lspci -k | grep -A 2 -E "(VGA|3D)"
 # my graphics is GTX 1660TI,so we will install "nvidia"
-sudo pacman -S nvidia
+yes | sudo pacman -S nvidia
 
 # install X.Org
-pacman -S xorg
-pacman -S xorg-xinit
-pacman -S server
+yes | pacman -S xorg xorg-xinit xorg-server
+# pacman -S xorg-xinit
+# pacman -S server
+
+# install pkgfile command
+yes | pacman -S pkgfile
+
+# install firefox and Chinese package
+yes | sudo pacman -S firefox firefox-i18n-zh-cn
+
 
 # install dwm
 # should copy dwm source code
 sudo make clean install
 # install acpi to inquire power information
-pacman -S acpi
+yes | pacman -S acpi
 
 # install st
 # should copy st source st
@@ -35,22 +37,23 @@ sudo make clean install
 
 # let terminal use Chinese
 # in .xinitrc add set
-export LANG=zh_CN.utf-8
-export LANGUAGE=zh_CN:en_US
+
+echo "export LANG=zh_CN.utf-8" >> .xinitrc
+echo "export LANGUAGE=zh_CN:en_US" >> .xinitrc
 
 # install Chinese font
-pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji
+yes | sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji
 # download path is /usr/share/fonts
 # maybe no need this command and I think this command is invalid  `sudo setfonts wqy_microhei.ttc`
 
 # install unzip
-pacman -S unzip
+yes | pacman -S unzip
 
 # install git
-pacman -S git
+yes | pacman -S git
 
 # install v2ray
-pacman -S v2ray
+yes | pacman -S v2ray
 # install v2raya
 # in AUR Wiki inquire v2raya URL
 git clone https://aur.archlinux.org/v2raya-bin/git
@@ -62,7 +65,7 @@ systemctl enable --now v2raya.service
 
 # install Shell
 # fish
-pacman -S fish
+yes | pacman -S fish
 # modify default shell
 chsh -s /usr/bin/fish
 # cancel welcome
@@ -71,24 +74,28 @@ set -U fish_greeting ""
 # curl -L https://git.oh-my.fish
 
 # install fcitx5
-pacman -S fcitx5-im
-pacman -S fcitx5-chinese-addons
+# pacman -S fcitx5-im
+# pacman -S fcitx5-chinese-addons
+yes | pacman -S fcitx5-im fcitx5-chinese-addons
 # write this word in .xinitrc
-export GTK_IM_MODULE=fictx5
-export QT_IM_MODULE=fcitx5
-export XMODIFIERS=@im=fcitx5
-fcitx5 &
+
+echo -en"\n\n\n\n"
+echo "# write this word in .xinitrc"
+echo "export GTK_IM_MODULE=fictx5"
+echo "export QT_IM_MODULE=fcitx5"
+echo "export XMODIFIERS=@im=fcitx5"
+echo "fcitx5 &"
 # modify fcitx5 fonts size
 # .config/fcitx5/conf/classicui.conf 
 # modify -> font="fontname fontsize"
 
 # install fcitx5 theme
-pacman -S fcitx5-nord
+# pacman -S fcitx5-nord
 # download file in /usr/share/fcitx5/themes/
-fcitx5-configtool
+# fcitx5-configtool
 
 # install compton
-pacman -S compton
+yes | pacman -S compton
 
 # Editor ~/.config/compton/compton.conf
 	# 其他窗口不透明度,较高时壁纸更清晰,
@@ -113,7 +120,7 @@ pacman -S compton
 	fade-exclude = [ ];
 
 # install mplayer
-pacman -S mplayer
+# yes | pacman -S mplayer
 # install wallpaper player xwinwrap
 # need cp wxinwrap source code
 # sudo make clean install
@@ -123,7 +130,10 @@ pacman -S mplayer
 # Modify .xinitrc add this word
 	# xwinwrap -fs -nf -ov -- mpv -wid WID --fps=59 --no-audio --loop --no-osc --no-osd-bar --input-vo-keyboard=no --really-quiet ~/wallpaper/bg.mp4 &
 	xwinwrap -fs -nf -ov -- mplayer -af volume=-200 -fps 24 -shuffle -loop 0 -wid WID -nolirc ~/wallpaper/bg.mp4 &
-
+yes | sudo pacman -S mplayer
+echo -en "\n\n\n\n"
+echo "# xwinwrap"
+echo "xwinwrap -fs -nf -ov -- mplayer -af volume=-200 -fps 24 -shuffle -loop 0 -wid WID -nolirc ~/wallpaper/bg.mp4 &"
 
 # modify nvim theme
 # Editor ~/.config/nvim/init.vim
@@ -134,7 +144,7 @@ pacman -S mplayer
 
 
 # install coc depend
-pacman -S nodejs npm 
+yes | sudo pacman -S nodejs npm 
 # use nvim command
 # install Coc-plug manager
 CocInstall coc-marketolace
@@ -142,10 +152,10 @@ CocInstall coc-clangd
 CocInstall coc-sh
 
 # install coc-python depend
-pacman S python-pylint jedi-language-server
+yes | pacman S python-pylint jedi-language-server
 CocInstall coc-python
 # if write python file have errer,please try update coc coc-plug vim-plug
 
 
 # install stranstion
-pacman -S translate-shell
+yes | sudo pacman -S translate-shell
