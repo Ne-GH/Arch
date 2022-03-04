c5c15f8a25da1ba91874159d0083fe795c3dba9f
永恒之蓝 <61956889+Ne-GH@users.noreply.github.com>
2022-03-04 22:37:12 +0800

    Update ConfigArch.sh

--- ConfigArch.sh
+++ ConfigArch.sh
@@ -11,8 +11,7 @@ YES | sudo pacman -S neovim
 
 YES | pacman -S xorg xorg-xinit xorg-server
 
-echo  "export LANG=zh_CN.utf-8" >> ~/.xinitrc
-echo  "export LANGUAGE=zh_CN:en_US" >> ~/.xinitrc
+
 
 # install dwm
 cd dwm
@@ -23,3 +22,15 @@ cd ..
 cd st
 sudo make clean install
 cd ..
+
+# install compton
+YES | sudo pacman -S compton
+
+echo  "export LANG=zh_CN.utf-8" >> ~/.xinitrc
+echo  "export LANGUAGE=zh_CN:en_US" >> ~/.xinitrc
+echo "# 分辨率" >> ~/.xinitrc
+echo -e "xrandr --newmode \"1920x1080_60\" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync" >> ~/.xinitrc
+echo -e "xrandr --addmode Virtual-1 \"1920x1080_60\"" >> ~/.xinitrc
+echo -e "xrandr --output Virtual-1 --mode \"1920x1080_60\"" >> ~/.xinitrc
+
+echo "exec dwm" >> ~/.xinitrc
