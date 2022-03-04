e7ce59fb4a6dbbd2e66bbbd9b5144d37da7ffe06
Ne-GH <printfne@gmail.com>
2022-03-04 17:58:58 +0800

    config

--- ConfigArch.sh
+++ ConfigArch.sh
@@ -38,8 +38,13 @@ sudo make clean install
 # let terminal use Chinese
 # in .xinitrc add set
 
-echo "export LANG=zh_CN.utf-8" >> .xinitrc
-echo "export LANGUAGE=zh_CN:en_US" >> .xinitrc
+echo "# 分辨率"
+echo -en "xrandr --newmode \"1920x1080_60\" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync"
+echo -en "xrandr --addmode Virtual-1 \"1920x1080_60\""
+echo -en "xrandr --output Virtual-1 --mode \"1920x1080_60\""
+
+# echo -en "export LANG=zh_CN.utf-8" >> .xinitrc
+# echo -en "export LANGUAGE=zh_CN:en_US" >> .xinitrc
 
 # install Chinese font
 yes | sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji
@@ -47,13 +52,13 @@ yes | sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji
 # maybe no need this command and I think this command is invalid  `sudo setfonts wqy_microhei.ttc`
 
 # install unzip
-yes | pacman -S unzip
+yes | sudo pacman -S unzip
 
 # install git
-yes | pacman -S git
+yes | sudo pacman -S git
 
 # install v2ray
-yes | pacman -S v2ray
+yes | sudo pacman -S v2ray
 # install v2raya
 # in AUR Wiki inquire v2raya URL
 git clone https://aur.archlinux.org/v2raya-bin/git
@@ -65,22 +70,22 @@ systemctl enable --now v2raya.service
 
 # install Shell
 # fish
-yes | pacman -S fish
+# yes | pacman -S fish
 # modify default shell
-chsh -s /usr/bin/fish
+# chsh -s /usr/bin/fish
 # cancel welcome
-set -U fish_greeting ""
+# set -U fish_greeting ""
 # install oh-my-fish
 # curl -L https://git.oh-my.fish
 
 # install fcitx5
 # pacman -S fcitx5-im
 # pacman -S fcitx5-chinese-addons
-yes | pacman -S fcitx5-im fcitx5-chinese-addons
+yes | sudo pacman -S fcitx5-im fcitx5-chinese-addons
 # write this word in .xinitrc
 
-echo -en"\n\n\n\n"
-echo "# write this word in .xinitrc"
+echo -en "\n\n\n\n"
+echo "# fcitx5"
 echo "export GTK_IM_MODULE=fictx5"
 echo "export QT_IM_MODULE=fcitx5"
 echo "export XMODIFIERS=@im=fcitx5"
@@ -95,32 +100,40 @@ echo "fcitx5 &"
 # fcitx5-configtool
 
 # install compton
-yes | pacman -S compton
+yes | sudo pacman -S compton
 
 # Editor ~/.config/compton/compton.conf
 	# 其他窗口不透明度,较高时壁纸更清晰,
-	inactive-opacity = 0.95;
-	# 当前聚焦窗口
-	active-opacity = 0.85;
+echo -en "
+inactive-opacity = 0.95;
+# 当前聚焦窗口
+active-opacity = 0.85;
 
-	opacity-rule = [
-	#xprop
-	"79:name *?= 'nvim'&&focused",
-	#nvim被使用
-	"49:name *?= 'nvim'&&!focused",
-	#nvim未被使用
-	"99:class_g = 'firefox'&&focused",
-	"99:class_g = 'firefox'&&!focused",
- 	]
+opacity-rule = [
+#xprop
+\"79:name *?= 'nvim'&&focused\",
+#nvim被使用
+\"49:name *?= 'nvim'&&!focused\",
+#nvim未被使用
+\"99:class_g = 'firefox'&&focused\",
+\"99:class_g = 'firefox'&&!focused\",
+]
 
-	fading = true;
-	fade-delta = 15;
-	fade-in-step = 0.05;
-	fade-out-step = 0.05;
-	fade-exclude = [ ];
+fading = true;
+fade-delta = 15;
+fade-in-step = 0.05;
+fade-out-step = 0.05;
+fade-exclude = [ ];" > ~/.config/compton/compton.conf
+
+
+echo -en "\n\n"
+
+echo "# compton"
+echo "compton -b &"
 
 # install mplayer
-# yes | pacman -S mplayer
+yes |sudo pacman -S mplayer
+
 # install wallpaper player xwinwrap
 # need cp wxinwrap source code
 # sudo make clean install
@@ -129,8 +142,6 @@ yes | pacman -S compton
 # makepkg -si
 # Modify .xinitrc add this word
 	# xwinwrap -fs -nf -ov -- mpv -wid WID --fps=59 --no-audio --loop --no-osc --no-osd-bar --input-vo-keyboard=no --really-quiet ~/wallpaper/bg.mp4 &
-	xwinwrap -fs -nf -ov -- mplayer -af volume=-200 -fps 24 -shuffle -loop 0 -wid WID -nolirc ~/wallpaper/bg.mp4 &
-yes | sudo pacman -S mplayer
 echo -en "\n\n\n\n"
 echo "# xwinwrap"
 echo "xwinwrap -fs -nf -ov -- mplayer -af volume=-200 -fps 24 -shuffle -loop 0 -wid WID -nolirc ~/wallpaper/bg.mp4 &"
@@ -147,9 +158,10 @@ echo "xwinwrap -fs -nf -ov -- mplayer -af volume=-200 -fps 24 -shuffle -loop 0 -
 yes | sudo pacman -S nodejs npm 
 # use nvim command
 # install Coc-plug manager
-CocInstall coc-marketolace
-CocInstall coc-clangd
-CocInstall coc-sh
+
+# CocInstall coc-marketolace
+# CocInstall coc-clangd
+# CocInstall coc-sh
 
 # install coc-python depend
 yes | pacman S python-pylint jedi-language-server
