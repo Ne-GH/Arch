0fd9e0058f209b2111386b379c1be8f9fdca84f6
永恒之蓝 <61956889+Ne-GH@users.noreply.github.com>
2022-03-04 22:26:11 +0800

    Update ConfigArch.sh

--- ConfigArch.sh
+++ ConfigArch.sh
@@ -10,3 +10,16 @@ YES | sudo pacman -S neovim
 # YES | sudo pacman -S nvidia
 
 YES | pacman -S xorg xorg-xinit xorg-server
+
+echo  "export LANG=zh_CN.utf-8" >> ~/.xinitrc
+echo  "export LANGUAGE=zh_CN:en_US" >> ~/.xinitrc
+
+# install dwm
+cd dwm
+sudo make clean install
+cd ..
+
+# install st
+cd st
+sudo make clean install
+cd ..
